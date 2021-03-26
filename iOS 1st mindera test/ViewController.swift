0e8f61f78a32164f928562d96af0c389b4625f62
stefanv.demoraes <stefanvargas3@gmail.com>
2021-03-26 07:08:44 -0300

    Initial Commit

---
+++ iOS 1st mindera test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS 1st mindera test
+//
+//  Created by Stefan V. de Moraes on 26/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
